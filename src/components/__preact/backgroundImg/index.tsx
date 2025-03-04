1f2e81b700fb8263902d163e0a83f5e169932d8f
Nathan Molina <itsjustnatemolina@gmail.com>
2025-03-03 22:42:18 -0300

    project initial setup

---
+++ src/components/__preact/backgroundImg/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./style.module.scss";
+
+interface IBackgroundIMG {};
+
+const BackgroundIMG = () => {
+
+    return (
+        <div className={styles.bgImgContainer} />
+    )
+};
+
+export default BackgroundIMG
